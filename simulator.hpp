23d4940c3f0ef1a2050d4180e53250e114663f75
Michael Bartling <michael.bartling15+dev@gmail.com>
2015-06-06 14:44:58 -0500

    Added basic simulator class

---
+++ simulator.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+#include <vector>
+#include <stdint>
+
+constexpr int ADDRESS_SPACE 1<<16;
+using std::cout;
+using std::endl;
+using std::vector;
+
+class simulator{
+public:
+
+private:
+  vector<uint16_t> memory(ADDRESS_SPACE);
+  vector<uint16_t> regs(8);
+  uint16_t N, Z, P;
+};
