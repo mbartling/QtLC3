2a8b1ebafb511c7a19b0956b142fe57b7554bd05
Jimmy Brisson <theotherjimmy@gmail.com>
2015-06-07 15:04:54 -0500

    fixed the BR tests to make sense

--- tests/Branch_unitTests.cpp
+++ tests/Branch_unitTests.cpp
@@ -77,7 +77,7 @@ TEST(CheckBR, AllNegative) {
    sim1.setReg(1, 0xFFFF);
    sim1.setReg(2, 0x1000);
    sim1.setReg(3, 0x0000);
-   uint16_t bTarget = (uint16_t) -4;
+   uint16_t bTarget = (uint16_t) (-4) & 0x1FF;
    //Simple NOP test
    sim1.setPC(0x3002);
    sim1.doInst(BR | bTarget);
@@ -112,23 +112,23 @@ TEST(CheckBR, AllNegative) {
    EXPECT_EQ(0x3000, sim1.getPC());
 
    sim1.setPC(0x3003);
-   sim1.setPcsrBit('p', true);
-   sim1.setPcsrBit('n', false);
+   sim1.setPcsrBit('n', true);
    sim1.setPcsrBit('z', false);
+   sim1.setPcsrBit('p', false);
    sim1.doInst(BR | SETN(1) | SETZ(0) | SETP(0) | bTarget);
    EXPECT_EQ(0x3000, sim1.getPC());
 
    sim1.setPC(0x3003);
+   sim1.setPcsrBit('n', false);
+   sim1.setPcsrBit('z', true);
    sim1.setPcsrBit('p', false);
-   sim1.setPcsrBit('n', true);
-   sim1.setPcsrBit('z', false);
    sim1.doInst(BR | SETN(0) | SETZ(1) | SETP(0) | bTarget);
    EXPECT_EQ(0x3000, sim1.getPC());
 
    sim1.setPC(0x3003);
-   sim1.setPcsrBit('p', false);
    sim1.setPcsrBit('n', false);
-   sim1.setPcsrBit('z', true);
+   sim1.setPcsrBit('z', false);
+   sim1.setPcsrBit('p', true);
    sim1.doInst(BR | SETN(0) | SETZ(0) | SETP(1) | bTarget);
    EXPECT_EQ(0x3000, sim1.getPC());
 
@@ -165,7 +165,7 @@ TEST(CheckBR, AllPositiveInstructions) {
    EXPECT_EQ(0x3005, sim1.getPC());
 
    sim1.setPC(0x3002);
-   sim1.doInst(ADDI | SETDR(4) | SETSR1(3) | ((uint16_t) -1) );  
+   sim1.doInst(ADDI | SETDR(4) | SETSR1(3) | (((uint16_t) -1) & 0x1F));  
    sim1.doInst(BR | SETN(1) | SETZ(1) | SETP(1) | 0x1);
    EXPECT_EQ(0x3005, sim1.getPC());
 
@@ -177,17 +177,17 @@ TEST(CheckBR, AllPositiveInstructions) {
 // ----------------------------------------
    sim1.setPC(0x3002);
    sim1.doInst(ADDI | SETDR(4) | SETSR1(3) | 1 );  
+   sim1.doInst(BR | SETN(0) | SETZ(0) | SETP(1) | 0x1);
+   EXPECT_EQ(0x3005, sim1.getPC());
+
+   sim1.setPC(0x3002);
+   sim1.doInst(ADDI | SETDR(4) | SETSR1(3) | (((uint16_t) -1) & 0x1F));  
    sim1.doInst(BR | SETN(1) | SETZ(0) | SETP(0) | 0x1);
    EXPECT_EQ(0x3005, sim1.getPC());
 
    sim1.setPC(0x3002);
-   sim1.doInst(ADDI | SETDR(4) | SETSR1(3) | ((uint16_t) -1) );  
-   sim1.doInst(BR | SETN(0) | SETZ(1) | SETP(0) | 0x1);
-   EXPECT_EQ(0x3005, sim1.getPC());
-
-   sim1.setPC(0x3002);
    sim1.doInst(ADDI | SETDR(4) | SETSR1(3) | 0 );  
-   sim1.doInst(BR | SETN(0) | SETZ(0) | SETP(1) | 0x1);
+   sim1.doInst(BR | SETN(0) | SETZ(1) | SETP(0) | 0x1);
    EXPECT_EQ(0x3005, sim1.getPC());
 }
 
