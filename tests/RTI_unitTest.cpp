9b949ab2d7d2ab708d6ec7233031a8348d9a7e5a
Jimmy Brisson <theotherjimmy@gmail.com>
2015-06-07 17:00:13 -0500

    added a test for unprivileged RTI

--- tests/RTI_unitTest.cpp
+++ tests/RTI_unitTest.cpp
@@ -29,4 +29,20 @@ TEST(CheckRTI, All) {
         EXPECT_EQ(sim4.getPcsrBit('p'), true);
         EXPECT_EQ(sim4.getPcsrBit('s'), true);
 
+        sim4.setReg(6, 0x3000);
+        sim4.memory[0x3000] = 0xDEAD;
+        sim4.memory[0x3001] = (1<<15) | (1<<2) | (1<<1) | (1<<0);
+        sim4.setPC(0x4000);
+        sim4.setPcsrBit('n',false);
+        sim4.setPcsrBit('z',false);
+        sim4.setPcsrBit('p',false);
+        sim4.setPcsrBit('s',true);
+        EXPECT_EQ(sim4.doInst(RTI), false);
+        EXPECT_EQ(sim4.getReg(6), 0x3000);
+        EXPECT_EQ(sim4.getPC(), 0x4001);
+        EXPECT_EQ(sim4.getPcsrBit('n'), false);
+        EXPECT_EQ(sim4.getPcsrBit('z'), false);
+        EXPECT_EQ(sim4.getPcsrBit('p'), false);
+        EXPECT_EQ(sim4.getPcsrBit('s'), true);
+
 }
