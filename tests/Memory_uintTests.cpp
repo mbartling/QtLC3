88a7fde3b96e884383e7a841d211a00eb40f867e
Jimmy Brisson <theotherjimmy@gmail.com>
2015-06-07 15:59:55 -0500

    fixed some of the LD tests

--- tests/Memory_uintTests.cpp
+++ tests/Memory_uintTests.cpp
@@ -30,19 +30,19 @@ TEST(CheckLD, AllPositive) {
 }
 
 TEST(CheckLD, ALLNegative) {
-        sim2.memory[0x3000 - 2] = 0xDEAD;
+        sim2.memory[0x3000 - 0x2] = 0xDEAD;
         sim2.setPC(0x3000 - 1);
 
-        sim2.doInst(LD | SETDR(1) | 0x2);
-        EXPECT_EQ(sim2.getReg(1), sim2.memory[0x3000 - 2]);
+        sim2.doInst(LD | SETDR(1) | (-0x2 & 0x1FF));
+        EXPECT_EQ(sim2.getReg(1), sim2.memory[0x3000 - 0x2]);
 
-        sim2.memory[0x3000 - 20] = 0xBEEF;
+        sim2.memory[0x3000 - 0x20] = 0xBEEF;
         sim2.setPC(0x3000 - 1);
-        sim2.doInst(LD | SETDR(2) | 0x20);
-        EXPECT_EQ(sim2.getReg(2), sim2.memory[0x3000 - 20]);
+        sim2.doInst(LD | SETDR(2) | (-0x20 & 0x1FF));
+        EXPECT_EQ(sim2.getReg(2), sim2.memory[0x3000 - 0x20]);
 
         sim2.memory[0x3000 - 0x100] = 0xCAFE;
         sim2.setPC(0x3000 - 1);
-        sim2.doInst(LD | SETDR(3) | 0x100);
+        sim2.doInst(LD | SETDR(3) | (-0x100 & 0x1FF));
         EXPECT_EQ(sim2.getReg(3), sim2.memory[0x3000 - 0x100]);
 }
