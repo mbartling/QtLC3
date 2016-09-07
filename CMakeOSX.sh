f84529f25e177881a263bc907d563a09808e6989
Michael Bartling <michael.bartling15@gmail.com>
2016-09-07 14:15:36 -0500

    Added a hacky script for building on mac

---
+++ CMakeOSX.sh
@@ -0,0 +1,6 @@
+mkdir -p build
+cd build
+#LDFLAGS:  -L/usr/local/opt/qt5/lib
+#    CPPFLAGS: -I/usr/local/opt/qt5/include
+cmake -DCMAKE_PREFIX_PATH=/usr/local/Cellar/qt5/5.6.1-1/ -DPYTHON_LIBRARY=/usr/local/Cellar/python/2.7.12/Frameworks/Python.framework/Versions/2.7/lib/libpython2.7.dylib ..
+make
