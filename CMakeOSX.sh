c970d6ac082328b8a3e16cb0c6bb2ffd32060e16
Michael Bartling <michael.bartling15@gmail.com>
2016-09-13 22:15:49 -0500

    IT FUCKING WORKS ON MAC and LINUX

--- CMakeOSX.sh
+++ CMakeOSX.sh
@@ -3,4 +3,4 @@ cd build
 #LDFLAGS:  -L/usr/local/opt/qt5/lib
 #    CPPFLAGS: -I/usr/local/opt/qt5/include
 cmake -DCMAKE_PREFIX_PATH="/usr/local/Cellar/qt5/5.6.1-1/;/usr/local/Cellar/python/2.7.12" -DPYTHON_LIBRARY=/usr/local/Cellar/python/2.7.12/Frameworks/Python.framework/Versions/2.7/lib/libpython2.7.dylib ..
-make
+make -j
